890efb5e8ee18d6a58811ba00917be3993bc7a69
Joseph Ross <joe@junelife.com>
2017-01-26 17:01:37 -0800

    Initial Commit

---
+++ SwiftSRP/SwiftSRP.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSRP.h
+//  SwiftSRP
+//
+//  Created by Joseph Ross on 1/26/17.
+//  Copyright © 2017 Joseph Ross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSRP.
+FOUNDATION_EXPORT double SwiftSRPVersionNumber;
+
+//! Project version string for SwiftSRP.
+FOUNDATION_EXPORT const unsigned char SwiftSRPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSRP/PublicHeader.h>
+
+
